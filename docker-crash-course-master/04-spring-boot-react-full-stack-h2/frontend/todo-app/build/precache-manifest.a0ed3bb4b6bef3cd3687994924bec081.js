f6c2e32f6f15923ecb1d2a5c8a79375d58a2c4ce
YimengL <yimengcsu@gmail.com>
2020-04-08 11:23:07 -0500

    add build

---
+++ docker-crash-course-master/04-spring-boot-react-full-stack-h2/frontend/todo-app/build/precache-manifest.a0ed3bb4b6bef3cd3687994924bec081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad49754fbb048b385f6f",
+    "url": "/static/css/main.d1ded855.chunk.css"
+  },
+  {
+    "revision": "ad49754fbb048b385f6f",
+    "url": "/static/js/main.005c11f2.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "90caeb2d4ad3fe625bd9296531e994b8",
+    "url": "/index.html"
+  }
+];
